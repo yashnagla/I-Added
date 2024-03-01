2ad632ac7d7d34e94403cd24d6a54824c089678e
yashnagla <naglayash@gmail.com>
2024-03-01 17:42:03 +0530

    Added JS File

---
+++ index.js
@@ -0,0 +1,5 @@
+const button1 = document.getElementById("dynamic-button-1");
+const button2 = document.getElementById("dynamic-button-2");
+
+button1.innerText = "Donate";
+button2.innerText = "Register";
