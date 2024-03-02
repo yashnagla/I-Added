be2db286d73b16197ccd5483fd959713ee507d95
yashnagla <naglayash@gmail.com>
2024-03-03 00:58:34 +0530

    Removed Zip code

--- index.js
+++ index.js
@@ -3,11 +3,3 @@ const button2 = document.getElementById("dynamic-button-2");
 
 button1.innerText = "Donate";
 button2.innerText = "Register";
-
-// Zip Code Section Of Registration Page
-const zipCodeInput = document.getElementById("zipCode");
-zipCodeInput.addEventListener("input", function() {
-  if (this.value.length !== 6) {
-    this.value = "Please Enter Correct Zip Code.";
-  }
-});
