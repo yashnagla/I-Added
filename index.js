ec3cd555db2a5f58fa74f1c7bc6d74838c39e393
yashnagla <naglayash@gmail.com>
2024-03-03 19:38:12 +0530

    Add Password Match Function

--- index.js
+++ index.js
@@ -3,3 +3,30 @@ const button2 = document.getElementById("dynamic-button-2");
 
 button1.innerText = "Donate";
 button2.innerText = "Register";
+
+// Sign-Up Button
+document.getElementById("signUpButton").addEventListener("click", function () {
+  window.location.href = "login.html";
+});
+// Function to check if passwords match
+const checkPasswords = () => {
+  const passwordInput = document.getElementById("passwordInput");
+  const confirmPasswordInput = document.getElementById("confirmPasswordInput");
+  // Check if passwords match and update the message accordingly
+  if (passwordInput.value !== confirmPasswordInput.value) {
+    confirmPasswordInput.setCustomValidity(
+      "Passwords do not match. Please re-enter your password."
+    );
+  } else {
+    confirmPasswordInput.setCustomValidity("");
+  }
+  // Return true if both inputs are valid, or false otherwise
+  return passwordInput.checkValidity() && confirmPasswordInput.checkValidity();
+};
+
+// Add event listener to the form submission
+document.getElementById("myForm").addEventListener("submit", (event) => {
+  if (!checkPasswords()) {
+    event.preventDefault(); // Prevent form submission if passwords don't match
+  }
+});
