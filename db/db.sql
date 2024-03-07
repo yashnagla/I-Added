3bd2b8ceb88dc30ec8c69ca5df69725a78cda6b0
yashnagla <naglayash@gmail.com>
2024-03-07 10:35:28 +0530

    Created

---
+++ db/db.sql
@@ -0,0 +1,4 @@
+Create database IADDED
+Use IADDED
+Create Table Register(Full_Name varchar(50), EMail_ID Varchar(50) Primary Key, Phone_Number int, Password1 Varchar(50), PasswordC Varchar(50))
+Create Table Login(EMail_ID Varchar(50) Primary Key, Password1 Varchar(50))
